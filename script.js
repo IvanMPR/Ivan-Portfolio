3bf23966f9151e39522ad5421d6601c707e4a87c
Ivan MPR <ivsanmas@gmail.com>
2021-11-11 22:49:34 +0100

    'contact me' button ripple effect added

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 'use-strict';
+
+// Ripple effect on 'Contact me' button
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', function (e) {
+  rippleButton(e, button);
+});
+
+function rippleButton(event, element) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  const circle = document.createElement('span');
+  circle.style.position = 'absolute';
+  circle.style.left = `${x}px`;
+  circle.style.top = `${y}px`;
+  circle.style.transform = 'translate(-50%, -50%)';
+  circle.style.width = '15rem';
+  circle.style.height = '15rem';
+  circle.style.borderRadius = '50%';
+  circle.style.backgroundColor = '#fff';
+  circle.style.opacity = '0.6';
+  circle.classList.add('ripple-btn');
+
+  element.appendChild(circle);
+  setTimeout(() => {
+    element.removeChild(circle);
+  }, 500);
+}
