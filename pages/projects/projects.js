55ad5d9b7e8024ac99e071841493d6a13e623445
Ivan MPR <ivsanmas@gmail.com>
2022-02-28 22:28:03 +0100

    dev

--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -1,21 +1,7 @@
 'use-strict';
-// Initialize
 
-// Toggle section content (projects page)
-
-const sectionTogglers = document.querySelectorAll('.section__subtitle');
-const graphicDesignSubsection = document.querySelector(
-  '.section__projects--graphic'
-);
-const webDevSubsection = document.querySelector('.section__projects--web');
-
-sectionTogglers.forEach(function (h3) {
-  h3.addEventListener('click', function (e) {
-    sectionTogglers.forEach(toggler => toggler.classList.remove('active'));
-
-    this.classList.add('active');
-
-    graphicDesignSubsection.classList.toggle('invisible');
-    webDevSubsection.classList.toggle('invisible');
-  });
+const toggler = document.getElementById('toggle');
+const body = document.querySelector('body');
+toggler.addEventListener('change', function () {
+  return (body.dataset.theme = `${this.checked ? 'light' : 'default'}`);
 });
