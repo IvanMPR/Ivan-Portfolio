d174139c0b2de1d0beb4381bee37afbea5beb23a
Ivan MPR <ivsanmas@gmail.com>
2022-03-09 20:40:24 +0100

    dev

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,11 +1,25 @@
 'use-strict';
 
-// Toggle section content (about page)
-const sectionTogglers = document.querySelectorAll('.section__subtitle');
+// const toggler = document.getElementById('toggle');
+const body = document.querySelector('body');
 
-sectionTogglers.forEach(function (h3) {
-  h3.addEventListener('click', function (e) {
-    sectionTogglers.forEach(toggler => toggler.classList.remove('h3-active'));
-    this.classList.add('h3-active');
-  });
+window.addEventListener('load', function () {
+  if (localStorage.getItem('theme') === 'light') {
+    // toggler.checked = true;
+    body.dataset.theme = 'light';
+  } else {
+    // toggler.checked = false;
+    body.dataset.theme = 'default';
+  }
+  console.log(localStorage);
 });
+
+// Toggle section content (about page)
+// const sectionTogglers = document.querySelectorAll('.section__subtitle');
+
+// sectionTogglers.forEach(function (h3) {
+//   h3.addEventListener('click', function (e) {
+//     sectionTogglers.forEach(toggler => toggler.classList.remove('h3-active'));
+//     this.classList.add('h3-active');
+//   });
+// });
