4e74c7a63c26f43e66d0e773d5efd8e0ec738aa4
Ivan MPR <ivsanmas@gmail.com>
2022-03-09 20:40:44 +0100

    dev

--- pages/projects/projects.js
+++ pages/projects/projects.js
@@ -1,29 +1,29 @@
 'use-strict';
 
-const toggler = document.getElementById('toggle');
+// const toggler = document.getElementById('toggle');
 const body = document.querySelector('body');
 
 window.addEventListener('load', function () {
   if (localStorage.getItem('theme') === 'light') {
-    toggler.checked = true;
+    // toggler.checked = true;
     body.dataset.theme = 'light';
   } else {
-    toggler.checked = false;
+    // toggler.checked = false;
     body.dataset.theme = 'default';
   }
   console.log(localStorage);
 });
 
-toggler.addEventListener('change', function () {
-  if (this.checked) {
-    localStorage.setItem('theme', 'light');
-    body.dataset.theme = 'light';
-  } else {
-    localStorage.setItem('theme', 'default');
-    body.dataset.theme = 'default';
-  }
-  console.log(localStorage);
+// toggler.addEventListener('change', function () {
+//   if (this.checked) {
+//     localStorage.setItem('theme', 'light');
+//     body.dataset.theme = 'light';
+//   } else {
+//     localStorage.setItem('theme', 'default');
+//     body.dataset.theme = 'default';
+//   }
+//   console.log(localStorage);
 
-  // localStorage.setItem('light', this.checked);
-  // return (body.dataset.theme = `${this.checked ? 'light' : 'default'}`);
-});
+// localStorage.setItem('light', this.checked);
+// return (body.dataset.theme = `${this.checked ? 'light' : 'default'}`);
+// });
